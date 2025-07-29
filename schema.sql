592311234e892cdde7ebceffad2019d166221466
OmkarLanke <95911892+OmkarLanke@users.noreply.github.com>
2025-07-29 19:32:29 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,9 +1,11 @@
-CREATE DATABASE journey;
+CREATE DATABASE IF NOT EXISTS journey;
      USE journey;
+     ALTER TABLE memories ADD COLUMN title VARCHAR(100) NOT NULL AFTER photo_path;
 
      CREATE TABLE memories (
          id INT AUTO_INCREMENT PRIMARY KEY,
          photo_path VARCHAR(255) NOT NULL,
+         title VARCHAR(100) NOT NULL,
          description TEXT NOT NULL,
          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
      );
